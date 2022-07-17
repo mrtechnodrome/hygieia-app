dfe4133a26b2464a46edcd0c2e027750beba64c6
mrtechnodrome <Krishna15112002@gmail.com>
2022-07-17 16:17:17 +0530

    Update MainContainer.jsx

--- src/components/MainContainer.jsx
+++ src/components/MainContainer.jsx
@@ -47,7 +47,24 @@ const MainContainer = () => {
         />
       </section>
 
-      <MenuContainer />
+      <MenuContainer>
+      <div className="hidden md:flex gap-3 items-center">
+            <motion.div
+              whileTap={{ scale: 0.75 }}
+              className="w-8 h-8 rounded-lg bg-orange-300 hover:bg-orange-500 cursor-pointer  hover:shadow-lg flex items-center justify-center"
+              onClick={() => setScrollValue(-200)}
+            >
+              <MdChevronLeft className="text-lg text-white" />
+            </motion.div>
+            <motion.div
+              whileTap={{ scale: 0.75 }}
+              className="w-8 h-8 rounded-lg bg-orange-300 hover:bg-orange-500 cursor-pointer transition-all duration-100 ease-in-out hover:shadow-lg flex items-center justify-center"
+              onClick={() => setScrollValue(200)}
+            >
+              <MdChevronRight className="text-lg text-white" />
+            </motion.div>
+          </div>
+      </MenuContainer>
 
       <section className="w-full my-6">
         <div className="w-full flex items-center justify-between">
